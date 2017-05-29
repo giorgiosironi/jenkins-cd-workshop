b2956cfd21522cd1c1e29b3742252b0554758f64
Giorgio Sironi <g.sironi@elifesciences.org>
2017-05-29 17:03:10 +0100

    Renaming image to Docker hub convention

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-sudo docker build -t giorgiosironi:jenkins-cd-workshop .
+sudo docker build -t giorgiosironi/jenkins-cd-workshop .
--- jenkins.sh
+++ jenkins.sh
@@ -5,5 +5,5 @@ sudo docker run \
 	-p 8080:8080 \
 	-v $(pwd)/jenkins_home:/var/jenkins_home \
 	--env JAVA_OPTS=-Djenkins.install.runSetupWizard=false \
-	giorgiosironi:jenkins-cd-workshop
+	giorgiosironi/jenkins-cd-workshop
 
