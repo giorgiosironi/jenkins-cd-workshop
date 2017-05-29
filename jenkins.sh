0b4e57bfdd148d6d8e3557f6e2a2dd3863d40e43
Giorgio Sironi <g.sironi@elifesciences.org>
2017-05-29 17:03:30 +0100

    Opening up SSH port for common steps repositories

--- jenkins.sh
+++ jenkins.sh
@@ -3,6 +3,7 @@ set -e
 
 sudo docker run \
 	-p 8080:8080 \
+    -p 16022:16022 \
 	-v $(pwd)/jenkins_home:/var/jenkins_home \
 	--env JAVA_OPTS=-Djenkins.install.runSetupWizard=false \
 	giorgiosironi/jenkins-cd-workshop
