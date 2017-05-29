7ada803fe2e5296642cc835a7c3155220ed30719
Giorgio Sironi <g.sironi@elifesciences.org>
2017-05-29 14:45:11 +0100

    Skipping initial setup wizard

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
 
-sudo docker run -p 8080:8080 -v $(pwd)/jenkins_home:/var/jenkins_home jenkins:2.46.3
+sudo docker run \
+	-p 8080:8080 \
+	-v $(pwd)/jenkins_home:/var/jenkins_home \
+	--env JAVA_OPTS=-Djenkins.install.runSetupWizard=false \
+	giorgiosironi:jenkins-cd-workshop
 
