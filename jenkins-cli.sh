a99aecaeb032ffa4848aab55a0f3c9ee38645d0b
Giorgio Sironi <g.sironi@elifesciences.org>
2017-06-21 16:54:31 +0100

    Replacing wget with curl for portability

--- jenkins-cli.sh
+++ jenkins-cli.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ ! -e jenkins-cli.jar ]; then
-    wget http://localhost:8080/jnlpJars/jenkins-cli.jar
+    curl -O http://localhost:8080/jnlpJars/jenkins-cli.jar
 fi
 
 java -jar jenkins-cli.jar -s http://localhost:8080 $@
